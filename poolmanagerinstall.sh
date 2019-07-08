fccdda6326cb9798c1d317f57e4f3fa09c269838
Daniel Little <daniel.r.little.1994@gmail.com>
2019-07-08 16:45:10 +0100

    removed new user

--- poolmanagerinstall.sh
+++ poolmanagerinstall.sh
@@ -2,13 +2,10 @@
 
 sudo apt update -y
 sudo apt upgrade -y
-sudo su useradd --create-home mongodb
-sudo usermod --shell /bin/bash mongodb
-sudo su - mongodb -c "curl -sL https://deb.nodesource.com/setup_10.x | sudo -E bash -"
+curl -sL https://deb.nodesource.com/setup_10.x | sudo -E bash -
 sudo apt install nodejs -y
 sudo apt install npm -y
 sudo apt install git -y
-cd /home/mongodb
 git clone https://github.com/yamileon/poolmanager-api.git
 cd poolmanager-api
 sudo npm install
