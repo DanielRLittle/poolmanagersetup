c67ac7e9a2b42732c06c95ebf577bfc474e407e1
Daniel Little <daniel.r.little.1994@gmail.com>
2019-07-08 15:41:21 +0100

    first commit

---
+++ poolmanagerinstall.sh
@@ -0,0 +1,17 @@
+#! /bin/bash
+
+sudo apt update -y
+sudo apt upgrade -y
+sudo su - mongodb -c "curl -sL https://deb.nodesource.com/setup_10.x | sudo -E bash -"
+sudo apt install nodejs -y
+sudo apt install npm -y
+sudo apt install git -y
+cd /home/mongodb
+git clone https://github.com/yamileon/poolmanager-api.git
+cd poolmanager-api
+sudo npm install
+sudo apt install mongodb -y
+git checkout dev
+cp ~/poolmangersetup/api.service /etc/systemd/system/
+sudo systemctl daemon-reload
+sudo systemctl start jenkins
