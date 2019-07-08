70054e1ff854ab27d300c9dfe787f2abc3a38552
Daniel Little <daniel.r.little.1994@gmail.com>
2019-07-08 16:34:26 +0100

    added a user mongodb

--- poolmanagerinstall.sh
+++ poolmanagerinstall.sh
@@ -2,6 +2,8 @@
 
 sudo apt update -y
 sudo apt upgrade -y
+sudo su useradd --create-home mongodb
+sudo usermod --shell /bin/bash mongodb
 sudo su - mongodb -c "curl -sL https://deb.nodesource.com/setup_10.x | sudo -E bash -"
 sudo apt install nodejs -y
 sudo apt install npm -y
