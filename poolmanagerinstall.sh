b1ec6171b17955d2f9d1ea76a0e61fc4de2a5517
hello <hello>
2019-07-09 09:39:30 +0000

    altered the service to add a user

--- poolmanagerinstall.sh
+++ poolmanagerinstall.sh
@@ -3,15 +3,13 @@
 sudo apt update -y
 sudo apt upgrade -y
 curl -sL https://deb.nodesource.com/setup_10.x | sudo -E bash -
-sudo apt install nodejs -y
-sudo apt install npm -y
-sudo apt install git -y
-sudo git clone https://github.com/yamileon/poolmanager-api.git
-sudo cd poolmanager-api/
-sudo ls
-sudo npm install
-sudo apt install mongodb -y
-sudo git checkout dev
+sudo useradd -m -s /bin/bash mongo
+sudo apt install -y npm git nodejs mongodb
+sudo su - mongo << EOF
+git clone https://github.com/yamileon/poolmanager-api.git
+cd poolmanager-api/ | sudo git checkout dev 
+npm install
+EOF
 sudo cp ~/poolmanagersetup/api.service /etc/systemd/system/
 sudo systemctl daemon-reload
 sudo systemctl enable api
