8a8fc69c4822779ffc4b851e1e893cf727305f29
Daniel Little <daniel.r.little.1994@gmail.com>
2019-07-09 09:54:49 +0100

    making sure the script is in the correct folder

--- poolmanagerinstall.sh
+++ poolmanagerinstall.sh
@@ -6,11 +6,12 @@ curl -sL https://deb.nodesource.com/setup_10.x | sudo -E bash -
 sudo apt install nodejs -y
 sudo apt install npm -y
 sudo apt install git -y
-git clone https://github.com/yamileon/poolmanager-api.git
-cd poolmanager-api
+sudo git clone https://github.com/yamileon/poolmanager-api.git
+sudo cd poolmanager-api/
+sudo ls
 sudo npm install
 sudo apt install mongodb -y
-git checkout dev
+sudo git checkout dev
 sudo cp ~/poolmanagersetup/api.service /etc/systemd/system/
 sudo systemctl daemon-reload
 sudo systemctl enable api
