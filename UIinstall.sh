59afd33c3040af7a797d19c61aa0cebbf2b01a50
Daniel Little <daniel.r.little.1994@gmail.com>
2019-07-11 14:50:52 +0100

    added yes no for npm install

--- UIinstall.sh
+++ UIinstall.sh
@@ -6,13 +6,13 @@ curl -sL https://deb.nodesource.com/setup_10.x | sudo -E bash -
 sudo seradd -m -s /bin/bash angular
 sudo apt install -y nodejs
 sudo apt install -y npm
-no | sudo npm install -g @angular/cli
+yes n | sudo npm install -g @angular/cli
 sudo su - angular <<EOF
 rm -rf poolmanage-ui
 git clone https://github.com/yamileon/poolmanage-ui.git
 cd poolmanage-ui/
 git checkout furr-dev2
-no | npm install
+yes n | npm install
 EOF
 sudo cp ~/poolmanagersetup/ui.service /etc/systemd/system/
 sudo systemctl daemon-reload
